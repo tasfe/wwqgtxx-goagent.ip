e7be99c5147f2ce781114cb9deaa37acc07b3116
wwqgtxx <wwqgtxx@gmail.com>
2013-01-29 20:44:37 +0800

    'bat'

--- check_google_ip.py
+++ check_google_ip.py
@@ -141,7 +141,7 @@ def main():
     common.write('Google Cn Ip:')
     common.writeline()
     common.writeconfig('google_cn','hosts','')
-    check_ip.run(__filename__,'203.208.',46,47)
+    #check_ip.run(__filename__,'203.208.',46,47)
     #check_ip.run(__filename__,'203.208.',46,47)
     common.writeips('google_cn','hosts')
     if common.getconfig('google_cn','hosts') == '' :
