f58a942666104d5ad88b21c48a42f2324650af73
wwqgtxx <wwqgtxx@gmail.com>
2013-01-29 20:18:35 +0800

    'bat'

--- update.py
+++ update.py
@@ -9,8 +9,8 @@ def main():
     print 'msg:', res.msg
     print 'headers:', res.getheaders()
     #html
-    #print '\n' + '-' * 50 + '\n'
-    #print res.read()
+    print '\n' + '-' * 50 + '\n'
+    print res.read()
     conn.close()
 
 if __name__ == '__main__':
