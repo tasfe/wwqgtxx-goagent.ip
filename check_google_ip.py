e776f3d3e6a5a75ab01523ccb3c3a51996a53100
wwqgtxx <wwqgtxx@gmail.com>
2013-01-29 20:00:25 +0800

    'bat'

--- check_google_ip.py
+++ check_google_ip.py
@@ -141,6 +141,7 @@ def main():
     common.writeline()
     common.writeconfig('google_cn','hosts','')
     check_ip.run(__filename__,'203.208.',46,47)
+    #check_ip.run(__filename__,'203.208.',46,47)
     common.writeips('google_cn','hosts')
     if common.getconfig('google_cn','hosts') == '' :
         print 'Can\'t Find Google Cn Ip,Change To Google_hk'
