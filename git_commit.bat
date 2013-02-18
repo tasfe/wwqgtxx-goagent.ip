7356454fd596bcc9b6be4490c42a9510aeecb9a4
wwqgtxx <wwqgtxx@gmail.com>
2013-02-18 09:53:46 +0800

    'bat'

--- git_commit.bat
+++ git_commit.bat
@@ -2,8 +2,8 @@ git config --global push.default matching
 git init
 git add .
 git commit -m'bat'
-start /min git push https://code.google.com/p/wwqgtxx.ip/ +master
-start /min git push https://code.google.com/p/wwqgtxx.ip2/ +master
+start /min git push https://code.google.com/p/wwqgtxx-wallproxy.ip/ +master
+start /min git push https://code.google.com/p/wwqgtxx-wallproxy.ip2/ +master
 start /min git push https://code.google.com/p/wwqgtxx-goagent.ip/ +master
 start /min git push https://code.google.com/p/wwqgtxx-goagent.ip2/ +master
 exit
